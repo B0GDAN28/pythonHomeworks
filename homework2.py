fc4e7646a5cd20912030fd4026a39ee711f64e7d
bogda <bogdan_opris2000@yahoo.com>
2023-03-02 11:00:23 +0200

    Homework2

--- homework2.py
+++ homework2.py
@@ -1,7 +1,22 @@
-car_brands=['BMW', 'AUDI', 'MERCEDES-BENZ', 'VOLKSWAGEN']
-for x in car_brands:
-    print(x)
-i = 30
-while i < 50:
-  print(i)
-  i += 1
+#car_brands=['BMW', 'AUDI', 'MERCEDES-BENZ', 'VOLKSWAGEN']
+#for x in car_brands:
+   # print(x)
+#i = 30
+#while i < 50:
+  #print(i)
+ # i += 1
+name=input('Enter youre name:  ')
+age=input('Enter youre age: ')
+height=input('Enter youre  height: ')
+
+try:
+      age=int(age)
+      height=int(height)
+except ValueError as e:
+    print(e)
+    print(f"You inserted '{age,height}' which is not a valid number!")
+
+print(f"Hello {name} , from our questions it was found that you are {age} years old and {height} cm")
+
+
+
