88bd0227f99d75bf9bbfdf5ff743d035668c4f0e
bogda <bogdan_opris2000@yahoo.com>
2023-03-03 14:58:46 +0200

    homework1 updated

--- Homework1.py
+++ Homework1.py
@@ -1,12 +1,11 @@
 my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
-my_list.sort() #initial list in ascedent
+my_list.sort()  # initial list in ascedent
 print(my_list)
-#my_list.reverse() # intial list in descendent
-#print(my_list)
-my_slice=my_list[0:10:2] # list in odd way by slice
+# my_list.reverse() # intial list in descendent
+# print(my_list)
+my_slice = my_list[0::2]  # list in odd way by slice
 print(my_slice)
-my_slice2=my_list[1:10:2] # list in par way by slice
+my_slice2 = my_list[1::2]  # list in par way by slice
 print(my_slice2)
-my_slice3=my_list[2:10:3] # Mutliples of 3
+my_slice3 = my_list[2::3]  # Mutliples of 3
 print(my_slice3)
-
