0e6c155dae0b21ed8b089f30093a65c7c4724402
bogda <bogdan_opris2000@yahoo.com>
2023-03-21 15:13:20 +0200

    homework4

--- Homework 3.5/Main.py
+++ Homework 3.5/Main.py
@@ -1,13 +1,14 @@
 # TODO #1: Account for `kwargs` and sum all the numbers in there
 def get_sum_from_kwargs(*args, **kwargs):
     total = 0
+    for arg in args:
+        if isinstance(arg, (int, float)):
+            total += arg
 
-    print(kwargs)
     for kwarg in kwargs.values():
 
         if isinstance(kwarg, (int, float)):
             total += kwarg
-            print(kwarg)
 
     return total
 
@@ -19,16 +20,3 @@ print(get_sum_from_kwargs(2, 4, 'abc', param_1=2, param_2=3))
 
 def get_sum_from_all(*args, **kwargs):
     total = 0
-    for arg in args:
-        if isinstance(arg, (int, float)):
-            total += arg
-    for kwarg in kwargs.values():
-
-        if isinstance(kwarg, (int, float)):
-            total += kwarg
-            print(kwarg)
-
-    return total
-
-
-print(get_sum_from_all(2, 4, 'abc', param_1=2, param_2=3))
