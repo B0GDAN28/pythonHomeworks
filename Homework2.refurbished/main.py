5f89a72e20739537817165cefe50258b6620481a
bogda <bogdan_opris2000@yahoo.com>
2023-03-21 15:12:18 +0200

    homework2 refurbished

--- Homework2.refurbished/main.py
+++ Homework2.refurbished/main.py
@@ -1,18 +1,4 @@
-# for _ in range(3):
-#     try:
-#         age = int(input("Age: "))
-#     except ValueError:
-#         print("Try again, you didn't write a number correctly")
-#
-#         continue
-#     if age == int(age):
-#         if age >= 18:
-#             print("You are a grown up!")
-#         else:
-#             print("You are a child!")
-#
-#     else:
-#         print("No more attempts")
+
 
 max_attempts = 3
 
