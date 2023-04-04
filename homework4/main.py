4698de8326cf24b79596711cec9d6a0db43011d4
bogda <bogdan_opris2000@yahoo.com>
2023-04-04 19:08:44 +0300

    homework4

--- homework4/main.py
+++ homework4/main.py
@@ -1,17 +1,8 @@
-from car_data import car_list
+from car_data.car_list import all_cars
 
 if __name__ == '__main__':
     # point 1
-    all_cars = [{"brand": "Opel", "Model": "Astra H", "hp": 150, "price": 2500},
-                {"brand": "Dacia", "Model": "Logan I", "hp": 100, "price": 900},
-                {"brand": "Bmw", "Model": "520d", "hp": 190, "price": 15000},
-                {"brand": "Ford", "Model": "Focus II", "hp": 130, "price": 2000},
-                {"brand": "Dacia", "Model": "Sandero", "hp": 90, "price": 700},
-                {"brand": "Volkswagen", "Model": "Passat", "hp": 170, "price": 5000},
-                {"brand": "Skoda", "Model": "Superb", "hp": 150, "price": 4000},
-                {"brand": "Audi", "Model": "A6", "hp": 190, "price": 13000},
-                {"brand": "Mercedes-Benz", "Model": "E-klasse", "hp": 190, "price": 17000},
-                {"brand": "Skoda", "Model": "Octavia", "hp": 140, "price": 3000}]
+
     for car in all_cars:
         car['id'] = id(car)
     # point 2
