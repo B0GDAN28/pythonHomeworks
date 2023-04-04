066cd90ee3a7153fb432f3564f5332a3a88498f9
bogda <bogdan_opris2000@yahoo.com>
2023-04-04 20:19:08 +0300

    homework4

--- homework4/main.py
+++ homework4/main.py
@@ -6,27 +6,27 @@ if __name__ == '__main__':
     for car in all_cars:
         car['id'] = id(car)
     # point 2
-    slow_cars = filter(lambda car0: car0["hp"] < 120, all_cars)
+    slow_cars = [car for car in all_cars if car["hp"] < 120]
     slow_cars = list(slow_cars)
     print("Slow cars are : ", slow_cars)
 
-    fast_cars = filter(lambda car1: 120 < car1["hp"] < 180, all_cars)
+    fast_cars = [car1 for car1 in all_cars if 120 < car1["hp"] < 180]
     fast_cars = list(fast_cars)
     print("Fast cars are: ", fast_cars)
 
-    sport_cars = filter(lambda car2: car2["hp"] >= 180, all_cars)
+    sport_cars = [car2 for car2 in all_cars if car2["hp"] >= 180]
     sport_cars = list(sport_cars)
     print("Sport cars are: ", sport_cars)
 
-    cheap_cars = filter(lambda car3: car3["price"] < 1000, all_cars)
+    cheap_cars = [car3 for car3 in all_cars if car3["price"] < 1000]
     cheap_cars = list(cheap_cars)
     print("Cars with price low are: ", cheap_cars)
 
-    medium_cars = filter(lambda car4: 1000 <= car4["price"] < 5000, all_cars)
+    medium_cars = [car4 for car4 in all_cars if 1000 <= car4["price"] < 5000]
     medium_cars = list(medium_cars)
     print("Cars with medium price are: ", medium_cars)
 
-    expensive_cars = price_high = filter(lambda car5: car5["price"] >= 5000, all_cars)
+    expensive_cars = [car5 for car5 in all_cars if car5["price"] >= 5000]
     expensive_cars = list(expensive_cars)
     print("Cars with price high are: ", expensive_cars)
 
@@ -42,5 +42,3 @@ if __name__ == '__main__':
 
     if not os.path.exists("output_data"):
         os.makedirs("output_data")
-
-
