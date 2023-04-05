dffa383ce1028bdac4ea492a2208a7c74ef8fc88
bogda <bogdan_opris2000@yahoo.com>
2023-04-05 21:53:05 +0300

    homework5

--- Homework5/main.py
+++ Homework5/main.py
@@ -6,17 +6,15 @@ class Fraction:
     def __str__(self):
         return f"Numaratorul este {self.numarator} si numitorul este {self.numitor}."
 
-
     def __add__(self, other):
-         comun = self.numitor * other.numitor
-         suma_numaratorilor = (self.numarator * other.numitor) + (other.numarator * self.numitor)
-         return Fraction(suma_numaratorilor, comun)
-
+        comun = self.numitor * other.numitor
+        suma_numaratorilor = (self.numarator * other.numitor) + (other.numarator * self.numitor)
+        return Fraction(suma_numaratorilor, comun)
 
 
 if __name__ == '__main__':
-    number1 = Fraction(12, 10,)
-    number2 = Fraction(30, 12,)
+    number1 = Fraction(12, 10, )
+    number2 = Fraction(30, 12, )
     result = number1 + number2
     print(result)
     # # number2=Fraction(10,30)
