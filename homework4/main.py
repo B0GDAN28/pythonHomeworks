4e54b1d6f0a831f47ae1626a05fb715d309ffaa6
bogda <bogdan_opris2000@yahoo.com>
2023-04-06 14:56:13 +0300

    homework4

--- homework4/main.py
+++ homework4/main.py
@@ -1,7 +1,7 @@
 from car_data.car_list import all_cars
 
 if __name__ == '__main__':
-    # point 1
+    # program1
 
     for car in all_cars:
         car['id'] = id(car)
@@ -24,25 +24,6 @@ if __name__ == '__main__':
     expensive_cars = [car5 for car5 in all_cars if car5["price"] >= 5000]
     print("Cars with price high are: ", expensive_cars)
 
-    # point 3
-    import csv
+    #  program2
 
-    filename = "input.csv"
-    header = ["BRAND", "MODEL", "HP", "PRICE"]
-    data = [
-        ["OPEL", "ASTRA H", 150, 2500],
-        ["DACIA", "LOGAN I", 100, 900],
-        ["BMW", "520D", 190, 15000],
-        ["FORD", "FOCUS II", 130, 2000],
-        ["DACIA", "SANDERO I", 90, 700],
-        ["VOLKSWAGEN", "PASSAT B6", 170, 5000],
-        ["SKODA", "SUPERB I", 150, 4000],
-        ["AUDI", "A6 C7", 190, 13000],
-        ["MERCEDES-BENZ", "E-KLASSE W212", 204, 170000],
-        ["SKODA", "OCTAVIA II", 140, 3000]
-
-    ]
-    with open(filename, 'w', newline="") as file:
-        csvwriter = csv.writer(file)
-        csvwriter.writerow(header)
-        csvwriter.writerows(data)
+    file = open("input.csv", "r")
