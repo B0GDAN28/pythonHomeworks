74065d8e48c1de29fecde01324c047c0f3e4c39e
bogda <bogdan_opris2000@yahoo.com>
2023-04-07 12:57:09 +0300

    homework4

--- homework4/main.py
+++ homework4/main.py
@@ -5,6 +5,15 @@ import json
 import uuid
 
 OUTPUT_DIRECTORY = "output_data"
+CATEGORIES = {
+    "slow_cars": lambda car: car["HP"] < 120,
+    "fast_cars": lambda car: 120 <= car["HP"] < 180,
+    "sport_cars": lambda car: car["HP"] > 180,
+    "cheap_cars": lambda car: car["PRICE"] < 1000,
+    "medium_cars": lambda car: 1000 <= car["PRICE"] < 5000,
+    "expensive_cars": lambda car: car["PRICE"] > 5000,
+
+}
 
 
 def clean_up_directories():
@@ -29,24 +38,27 @@ def get_input():
 
 
 def process_data(cars: list):
-    slow_cars = [car for car in cars if car["HP"] < 120]
-    fast_cars = [car1 for car1 in cars if 120 <= car1["HP"] < 180]
-    sport_cars = [car2 for car2 in cars if car2["HP"] >= 180]
-    cheap_cars = [car3 for car3 in cars if car3["PRICE"] < 1000]
-    medium_cars = [car4 for car4 in cars if 1000 <= car4["PRICE"] < 5000]
-    expensive_cars = [car5 for car5 in cars if car5["PRICE"] > 5000]
+    # slow_cars = [car for car in cars if car["HP"] < 120]
+    # fast_cars = [car1 for car1 in cars if 120 <= car1["HP"] < 180]
+    # sport_cars = [car2 for car2 in cars if car2["HP"] >= 180]
+    # cheap_cars = [car3 for car3 in cars if car3["PRICE"] < 1000]
+    # medium_cars = [car4 for car4 in cars if 1000 <= car4["PRICE"] < 5000]
+    # expensive_cars = [car5 for car5 in cars if car5["PRICE"] > 5000]
+    #
+    # write_to_file("slow_cars.json", slow_cars)
+    # write_to_file("fast_cars.json", fast_cars)
+    # write_to_file("sports_cars.json", sport_cars)
+    # write_to_file("cheap_cars.json", cheap_cars)
+    # write_to_file("medium_cars.json", medium_cars)
+    # write_to_file("expensive_cars.json", expensive_cars)
+    for key, condition_function in CATEGORIES.items():
+        data = [car for car in cars if condition_function(car)]
+        write_to_file(f"{key}.json", data)
 
-    write_to_file("slow_cars.json", slow_cars)
-    write_to_file("fast_cars.json", fast_cars)
-    write_to_file("sports_cars.json", sport_cars)
-    write_to_file("cheap_cars.json", cheap_cars)
-    write_to_file("medium_cars.json", medium_cars)
-    write_to_file("expensive_cars.json", expensive_cars)
 
-
-def write_to_file(file_name: str, cars: list):
+def write_to_file(file_name: str, cars1: list):
     with open(f"output_data/{file_name}", "w") as file:
-        json.dump(cars, file, indent=2)
+        json.dump(cars1, file, indent=2)
 
 
 if __name__ == '__main__':
