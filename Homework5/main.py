10ce922cd7770186253b6dc5503d7d55d8d899e8
bogda <bogdan_opris2000@yahoo.com>
2023-05-02 12:37:57 +0300

    homework5

--- Homework5/main.py
+++ Homework5/main.py
@@ -1,32 +1,59 @@
+import math
+
+
 class Fraction:
     def __init__(self, numerator, denominator):
-        self.numerator = numerator
-        self.denominator = denominator
+        if type(numerator) != int or type(denominator) != int:
+            raise TypeError("Numerator and denominator should be integer!")
+
+        if denominator == 0:
+            raise ZeroDivisionError
+
+        gcd = math.gcd(numerator, denominator)
+        self.__numerator = numerator // gcd
+        self.__denominator = denominator // gcd
 
     def __str__(self):
-        return f"Numaratorul este {self.numerator} si numitorul este {self.denominator}."
+        return f"{self.__numerator}/{self.__denominator}"
+
+    def __eq__(self, other):
+        if type(other) == int:
+            other = Fraction(other, 1)
+        elif type(other) != Fraction:
+            raise TypeError(f"Cannot compare fraction with {type(other).__name__}")
+
+        return self.__numerator == other.__numerator and self.__denominator == other.__denominator
+
+    def __float__(self):
+        return self.__numerator / self.__denominator
 
     def __add__(self, other):
-        comun = self.denominator * other.denominator
-        sum_numerators = (self.numerator * other.denominator) + (other.numerator * self.denominator)
-        return Fraction(sum_numerators, comun)
+        if type(other) == int:
+            other = Fraction(other, 1)
+        elif type(other) != Fraction:
+            raise TypeError(f"Cannot add fraction with {type(other).__name__}")
+
+        new_numerator = self.__numerator * other.__denominator + other.__numerator * self.__denominator
+        new_denominator = self.__denominator * other.__denominator
+        return Fraction(new_numerator, new_denominator)
 
     def __sub__(self, other):
-        comun1 = self.denominator * other.denominator
-        down_numerators = (self.numerator * other.denominator) - (other.numerator * self.denominator)
-        return Fraction(down_numerators, comun1)
+        if type(other) == int:
+            other = Fraction(other, 1)
+        elif type(other) != Fraction:
+            raise TypeError(f"Cannot sub fraction with {type(other).__name__}")
+
+        new_numerator = self.__numerator * other.__denominator - other.__numerator * self.__denominator
+        new_denominator = self.__denominator * other.__denominator
+        return Fraction(new_numerator, new_denominator)
 
     def inverse(self):
-            self.numerator, self.denominator = self.denominator, self.numerator
+        if self.__numerator == 0:
+            raise TypeError("We cannot create the inverse of this function because numerator is 0.")
+
+        return Fraction(self.__denominator, self.__numerator)
 
 
 if __name__ == '__main__':
-    number1 = Fraction(40, 10, )
-    print(number1)
-    number2 = Fraction(30, 12, )
-    result = number1 + number2
-    print(result)
-    result1 = number1 - number2
-    print(result1)
-    Fraction.inverse(number1)
-    print(number1)
+    x = Fraction(8, 4)
+    print(x)
